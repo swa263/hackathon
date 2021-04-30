5b491b70f04c3cfe40e4e3c2a88cf0f14a1d7594
swa263 <60567865+swa263@users.noreply.github.com>
2021-05-01 03:26:15 +0530

    Add files via upload

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,1 +1,7 @@
-
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/coronaprecautions",{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    console.log("connection succesful");}).catch((error)=>{console.log("error");})
